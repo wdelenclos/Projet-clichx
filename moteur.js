5eaada6da73a063f62bcda35b8b4c5563e058b75
Wladimir Delenclos <wdelenclos@gmail.com>
2016-05-26 10:19:23 +0200

    script help

--- moteur.js
+++ moteur.js
@@ -1,3 +1,7 @@
 /**
  * Created by Wladimir on 26/05/2016.
  */
+$( document ).ready(function() {
+    var modif = document.lastModified;
+    console.log( "Jquery chargé, version " +jQuery.fn.jquery + " Dernière modif:" + modif );
+});
