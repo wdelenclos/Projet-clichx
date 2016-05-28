8bcd63e1c8e451c1c1dc24ab10ee706ee20643e3
Wladimir Delenclos <wdelenclos@gmail.com>
2016-05-28 22:04:07 +0200

    footer

--- script/moteur.js
+++ script/moteur.js
@@ -12,25 +12,25 @@ $( document ).ready(function() {
 });
 $(document).ready(function(){
 
-    $(".buddy").on("swiperight",function(){
+    $(".card").on("swiperight",function(){
         $(this).addClass('rotate-left').delay(700).fadeOut(1);
-        $('.buddy').find('.status').remove();
+        $('.card').find('.status').remove();
 
         $(this).append('<div class="status like">Like!</div>');
         if ( $(this).is(':last-child') ) {
-            $('.buddy:nth-child(1)').removeClass ('rotate-left rotate-right').fadeIn(300);
+            $('.card:nth-child(1)').removeClass ('rotate-left rotate-right').fadeIn(300);
         } else {
             $(this).next().removeClass('rotate-left rotate-right').fadeIn(400);
         }
     });
 
-    $(".buddy").on("swipeleft",function(){
+    $(".card").on("swipeleft",function(){
         $(this).addClass('rotate-right').delay(700).fadeOut(1);
-        $('.buddy').find('.status').remove();
+        $('.card').find('.status').remove();
         $(this).append('<div class="status dislike">Dislike!</div>');
 
         if ( $(this).is(':last-child') ) {
-            $('.buddy:nth-child(1)').removeClass ('rotate-left rotate-right').fadeIn(300);
+            $('.card:nth-child(1)').removeClass ('rotate-left rotate-right').fadeIn(300);
             alert(' ');
         } else {
             $(this).next().removeClass('rotate-left rotate-right').fadeIn(400);
