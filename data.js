a97dbf9bad598fe9861b35b91359bdcdcbab8a89
Wladimir Delenclos <wdelenclos@gmail.com>
2016-05-29 15:57:48 +0200

    Database Json

--- data.js
+++ data.js
@@ -1,3 +1,10 @@
 /**
  * Created by Wladimir on 29/05/2016.
  */
+
+
+var lescliches = '{' +
+    ' "employees" : [' +
+    '{ "firstName":"John" , "lastName":"Doe" },' +
+    '{ "firstName":"Anna" , "lastName":"Smith" },' +
+    '{ "firstName":"Peter" , "lastName":"Jones" } ]}';
